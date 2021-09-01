b1d7cf959b4881c4691391bf6d62fb504571ba9f
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-09-01 10:37:24 +0300

    delete Feedback components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
+import PropTypes from "prop-types";
 import { Component } from "react";
-import Feedback from "./components/Feedback/Feedback";
+import Statistics from "./components/Statistics";
+import FeedbackOptions from "./components/FeedbackOptions";
+import Section from "./components/Section";
+import Notification from "./components/Notification";
 
 class App extends Component {
   state = {
@@ -35,15 +39,43 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Feedback
-          state={this.state}
-          countTotalFeedback={this.countTotalFeedback}
-          countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
-          onLeaveFeedback={this.onLeaveFeedback}
-        />
+        <header className="feedback">
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              options={this.state}
+              onLeaveFeedback={this.onLeaveFeedback}
+            />
+          </Section>
+
+          <Section title="Statistics">
+            {this.countTotalFeedback(this.state) === 0 ? (
+              <Notification message="No feedback given" />
+            ) : (
+              <Statistics
+                good={this.state.good}
+                neutral={this.state.neutral}
+                bad={this.state.bad}
+                total={this.countTotalFeedback(this.state)}
+                positivePercentage={this.countPositiveFeedbackPercentage(
+                  this.state
+                )}
+              />
+            )}
+          </Section>
+        </header>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  state: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+  }),
+  countTotalFeedback: PropTypes.func,
+  countPositiveFeedbackPercentage: PropTypes.func,
+};
 
 export default App;
